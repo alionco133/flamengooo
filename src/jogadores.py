88218a3cb3c15be98b8d1c6182e68116eb815a46
alionco133 <148788082+alionco133@users.noreply.github.com>
2026-05-12 20:27:10 -0300

    Create jogadores.py

---
+++ src/jogadores.py
@@ -0,0 +1,16 @@
+jogadores = [
+    "Arrascaeta",
+    "Pedro",
+    "Gerson",
+    "Bruno Henrique",
+    "De La Cruz",
+    "Everton Cebolinha",
+    "Luiz Araujo",
+    "Ayrton Lucas",
+    "Leo Pereira",
+    "Fabrício Bruno",
+    "Wesley",
+    "Rossi"
+]
+
+print("Sistema do Flamengo iniciado!")
